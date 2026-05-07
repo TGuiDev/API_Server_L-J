b5aaa7aea3967a0809ac9eb5f92315fe320fe201
Gui.Dev <81177448+TGuiDev@users.noreply.github.com>
2026-05-07 17:05:10 -0300

    Setando porta e ip da maneira certa

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.PORT) || 3000;
 
   // Configurar CORS para aceitar localhost em qualquer porta + futuro Android
   app.enableCors({
@@ -21,7 +22,7 @@ async function bootstrap() {
     transform: true,
   }));
 
-  await app.listen(process.env.PORT);
-  console.log(`✅ Server is running on http://localhost:${process.env.PORT}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`✅ Server is running on http://localhost:${port}`);
 }
 bootstrap();
